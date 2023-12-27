7f23626a568f0547d2c5437556292bd00abda620
almono <pwalczykiewicz93@gmail.com>
2023-12-27 21:51:38 +0100

    Restricting player movement in viewport

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,26 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float paddingLeft = 0.5f, paddingRight = 0.5f, paddingTop = 3f, paddingBottom = 2f;
 
     Vector2 rawInput;
+    Vector2 minBounds, maxBounds; // for restricting player to move only inside the camera viewport
+
+    private void Start()
+    {
+        InitBounds();
+    }
 
     void Update()
     {
         MovePlayer();
+    }
+
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0, 0)); // left bottom of screen
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));
     }
 
     void OnMove(InputValue value)
@@ -22,7 +36,12 @@ public class Player : MonoBehaviour
 
     void MovePlayer()
     {
-        Vector3 deltaPosition = rawInput * moveSpeed * Time.deltaTime; // time delta time is frame independent
-        transform.position += deltaPosition;
+        Vector2 deltaPosition = rawInput * moveSpeed * Time.deltaTime; // time delta time is frame independent
+
+        Vector2 newPosition = new Vector2();
+        newPosition.x = Mathf.Clamp(transform.position.x + deltaPosition.x, minBounds.x + paddingLeft, maxBounds.x - paddingRight);
+        newPosition.y = Mathf.Clamp(transform.position.y + deltaPosition.y, minBounds.y + paddingBottom, maxBounds.y - paddingBottom);
+
+        transform.position = newPosition;
     }
 }
